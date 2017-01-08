03aa248c1a2f23b543e70ba048560061ee07e4ef
gage <glezin87@gmail.com>
2017-01-07 19:56:18 -0500

    Adding document compare using hashes

--- doc_compare/doc_comp_1.pl
+++ doc_compare/doc_comp_1.pl
@@ -28,15 +28,40 @@ sub count_word_frq {
 
 	foreach my $entry (@{$file_data}) {
 		my $line = lc($entry);
-		#$line =~ s/\n//;
+		#$line =~ s/^[alpahnum]//g;
 		#print "====".$line."===\n";
 	 	my @words = split(/ /,$line);
 		foreach my $wentry (@words) {
-			print $wentry . "\n";
+			#ignore any words that match a non-word character
+			if (($wentry !~ /\W/) and ($wentry !~ /^$/)) {
+				#print $wentry . "\n";
+				#check if the work is a key in the hash
+				if (exists $word_frq{$wentry} ){
+					$word_frq{$wentry} = $word_frq{$wentry} + 1;
+				} else {
+					$word_frq{$wentry} = 1;	
+				}
+			}
 		}
 	}
 	
 	return %word_frq
+}
+
+#In hash references of counted words for f1, a second hash ref for counted words for f2
+#Out the radians of document angle
+#This is a demonstartion of how to de reference a hash
+sub calculate_angle{
+	my $f1_hash_ref = shift @_;
+	my $f2_hash_ref = shift @_;
+	my %f1_hash = %$f1_hash_ref;
+	my %f2_hash = %$f2_hash_ref;
+
+#	foreach my $key (sort keys %f2_hash){
+#	       my $key_str = $key;
+#	       print "The count is : " .$f2_hash{$key}." for the word : ".$key_str."\n";
+#	}
+	
 }
 
 my $file1 = "doc1.txt";
@@ -58,5 +83,12 @@ $fh2 = shift(@handles);
 @fh2_data_raw = <$fh2>;
 
 %frq_count_f1 = count_word_frq ( \@fh1_data_raw );
-#%frq_count_f2 = count_word_frq ( @fh2_data_raw );
+%frq_count_f2 = count_word_frq ( \@fh2_data_raw );
 
+#print the values of the hash for testing
+#foreach my $key (sort keys %frq_count_f2){
+#	my $key_str = $key;
+#	print "The count is : " .$frq_count_f2{$key}." for the word : ".$key_str."\n";
+#}
+
+calculate_angle(\%frq_count_f1,\%frq_count_f2);
