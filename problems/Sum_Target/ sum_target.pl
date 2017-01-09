2e408bd6aed72727718cfc396d72dc8b4a00ef22
twelvegage157 <glezin87@gmail.com>
2017-01-09 11:04:18 -0500

    Create  sum_target.pl

---
+++ problems/Sum_Target/ sum_target.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+# Given a list of numbers, find a pair of numbers that sum to a target
+# Starting at position 0, build a hash of current array value and what is needed.
+# Check if what is needed to meet the target exists in the hash.
+# If it is found, you are done.
+# Other wise add the next array valued to the hash and what is needed for that value to meet the target.
