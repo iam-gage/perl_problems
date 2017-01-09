e0d353b518e3e24b3972eb831c4eda672000abde
twelvegage157 <glezin87@gmail.com>
2017-01-09 14:49:07 -0500

    Update sum_target.pl
    completed solution

--- problems/Sum_Target/sum_target.pl
+++ problems/Sum_Target/sum_target.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
+use warnings;
+use strict;
 
 # Given a list of numbers, find a pair of numbers that sum to a target
 # Starting at position 0, build a hash of current array value and what is needed.
-# Check if what is needed to meet the target exists in the hash.
 # If it is found, you are done.
 # Other wise add the next array valued to the hash and what is needed for that value to meet the target.
 
@@ -14,10 +15,32 @@
 # < array value n >
 
 sub read_input {
-   my $file_name = shift @_;
-   my $file_handle;
-   open ( $file_handle, '<' , $file_name ) or die "could not open the input file $file_name \n";
-   return <$file_handle> ;
+        my $file_name = shift @_;
+        my $file_handle;
+        open ( $file_handle, '<' , $file_name ) or die "could not open the input file $file_name \n";
+        my @result = <$file_handle> ;
+        return @result;
 }
 
+my %needed_value_hash;
 my @data = read_input ( "input_file_sum_target_0.txt" );
+my $target = shift @data;
+my @clean_data = grep (/\d/, @data);
+
+foreach my $entry ( @clean_data ) {
+        my $second_operand = abs ( $target - $entry );
+                $entry =~ s/\n//;
+                # build the hash of the needed operand for each array entry
+                if ( not exists $needed_value_hash{$entry} ) {
+                        $needed_value_hash{$entry} = $second_operand ;
+                }
+                # Demonstration of using the thin arrow to access hash entries
+                if ( exists $needed_value_hash{$second_operand} ){
+                        my $hashref = \%needed_value_hash;
+                        print $entry . " *** \n";
+                        print $hashref -> {$entry} . "\n";
+                        print $hashref -> {$second_operand}. "\n";
+                        exit;
+                }
+        }
+
